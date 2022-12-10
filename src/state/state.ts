235aeba7b9a29cd7520c8f5ee4788d93f3ee5831
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-10 12:56:54 +0200

    Add snakeHead and cellSize to the state

--- src/state/state.ts
+++ src/state/state.ts
@@ -3,9 +3,11 @@ import { State } from "../types/state";
 export const state: State = {
     gridWidth: 10,
     gridHeight: 10,
+    cellSize: 10,
     direction: undefined,
     snakeBody: [{ x: 8, y: 5 }],
+    snakeHead: { x: 5, y: 5 },
     gameOver: false,
-    cherry: { x1: 5, y1: 5 },
+    cherry: { x2: 5, y2: 5 },
     teleportSnake: false,
 };
--- src/types/state.ts
+++ src/types/state.ts
@@ -1,10 +1,12 @@
 export interface State {
     gridWidth: number;
     gridHeight: number;
+    cellSize: number;
     direction: undefined | string;
+    snakeHead: { x: number; y: number };
     snakeBody: Array<Object>;
     gameOver: boolean;
-    cherry: { x1: number; y1: number };
+    cherry: { x2: number; y2: number };
     teleportSnake: boolean;
 }
 export interface CellProps {
