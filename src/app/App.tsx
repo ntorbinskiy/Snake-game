c4dfe0314102ad8595d55fb3318777318064d5b4
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-10 11:36:06 +0200

    git init

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Board from "../components/board/Board";
+function App() {
+    return (
+        <div className="app">
+            <Board />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/cherry/Cherry.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cherry = () => {
+    return <div></div>;
+};
+
+export default Cherry;
