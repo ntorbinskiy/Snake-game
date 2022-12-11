91903d8623e0f341e87c2f1f2f141ec3622a16bf
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-11 13:53:12 +0200

    Change gridWidth and gridHeight

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,8 +1,8 @@
 import { State } from "../types/state";
 
 export const state: State = {
-    gridWidth: 10,
-    gridHeight: 10,
+    gridWidth: 5,
+    gridHeight: 5,
     direction: undefined,
     snakeBody: [{ x: 8, y: 5 }],
     snakeHead: { x: 5, y: 5 },
