c28ae0613ef27b846b8965e5bba5200d74413687
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-11 13:52:17 +0200

    Change size if cells
    Add Snake to the Cell

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import "./Board.css";
 import { CellProps, CellsProps } from "../../types/state";
 import { state } from "../../state/state";
+import Snake from "../Snake/Snake";
 
 const Cell = ({ x, y }: CellProps) => {
-    return <div className="cell"></div>;
+    return (
+        <div className="cell">
+            <Snake />
+        </div>
+    );
 };
 
 const Cells = ({ gridHeight, gridWidth }: CellsProps) => {
     const result = [];
     for (let y = 0; y < gridWidth; y++) {
         for (let x = 0; x < gridHeight; x++) {
-            result.push(<Cell key={`${x}_${y}`} x={x} y={y} />);
+            result.push(<Cell key={`${x}_${y}`} x={x} y={y}></Cell>);
         }
     }
     return <>{result}</>;
@@ -22,8 +27,12 @@ const Board = () => {
         <div
             className="board"
             style={{
-                gridTemplateColumns: ` repeat(${state.gridHeight}, 1fr)`,
-                gridTemplateRows: ` repeat(${state.gridWidth}, 1fr)`,
+                gridTemplateColumns: ` repeat(${state.gridHeight}, ${
+                    state.gridWidth / state.gridHeight
+                }fr)`,
+                gridTemplateRows: ` repeat(${state.gridWidth}, ${
+                    state.gridWidth / state.gridHeight
+                }fr)`,
             }}
         >
             <Cells gridHeight={state.gridHeight} gridWidth={state.gridWidth} />
