cc12408b06ae03d7de8b6f00bc4bd649b74d3eb3
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-11 13:15:19 +0200

    Add snake head

--- src/components/Snake/Snake.tsx
+++ src/components/Snake/Snake.tsx
@@ -1,4 +1,13 @@
 import React from "react";
+import "./Snake.css";
+
+interface SnakeHeadProps {
+    snakeHead: { x: number; y: number };
+}
+const SnakeHead = ({ snakeHead }: SnakeHeadProps) => {
+    const { x, y } = snakeHead;
+    return <div className="snake-head" style={{}}></div>;
+};
 
 const Snake = () => {
     return <div></div>;
