8c5efd7312cca837926f517b38a27be5796a7d79
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-28 22:14:32 +0200

    Add state on the top of app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
 import Board from "../components/Board/Board";
+import { useEffect } from "react";
+import timerTick from "../logic/tick";
+import { state } from "../state/state";
 function App() {
+    useEffect(() => {
+        timerTick();
+    }, []);
+
     return (
         <div className="app">
-            <Board />
+            <Board state={state} />
         </div>
     );
 }
