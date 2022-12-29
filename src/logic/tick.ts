b6128d4de328cefcca316c51bec53ce361a769dd
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-29 14:31:13 +0200

    Remove logic folder

--- src/logic/tick.ts
+++
@@ -1,9 +0,0 @@
-import { moveSnake } from "./movement";
-
-const timerTick = () => {
-    return setInterval((event: Event) => {
-        moveSnake(event);
-    }, 1000);
-};
-
-export default timerTick;
