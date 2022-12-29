25fd292d7a0a9f6c21e405a77ef0d806ef2263a1
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-29 14:24:30 +0200

    Remove inline width and height of Snake components

--- src/components/Snake/Snake.tsx
+++ src/components/Snake/Snake.tsx
@@ -3,24 +3,9 @@ import React from "react";
 import "./Snake.css";
 
 export const SnakeHead = () => {
-    return (
-        <div
-            className="snake-head"
-            style={{
-                height: `100%`,
-                width: `100%`,
-            }}
-        ></div>
-    );
+    return <div className="snake snake-head"></div>;
 };
+
 export const SnakeBody = () => {
-    return (
-        <div
-            className="snake-body"
-            style={{
-                height: `100%`,
-                width: `100%`,
-            }}
-        ></div>
-    );
+    return <div className="snake snake-body"></div>;
 };
