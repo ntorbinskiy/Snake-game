7b8095a102e82e1064bd8b307ad6a8553076d11b
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-29 14:27:32 +0200

    App component refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,28 @@
 import "./App.css";
 import Board from "../components/Board/Board";
-import { useEffect } from "react";
-import timerTick from "../logic/tick";
-import { state } from "../state/state";
+import { createInitialState } from "../engine/engine";
+
+export const UP = "UP";
+export const DOWN = "DOWN";
+export const LEFT = "LEFT";
+export const RIGHT = "RIGHT";
+
+type MovementKeys = typeof UP | typeof DOWN | typeof LEFT | typeof RIGHT;
+
+const movementKeyCodes: Record<string, MovementKeys> = {
+    ArrowUp: UP,
+    ArrowDown: DOWN,
+    ArrowLeft: LEFT,
+    ArrowRight: RIGHT,
+};
+
 function App() {
-    useEffect(() => {
-        timerTick();
-    }, []);
+    const state = createInitialState({
+        gridWidth: 5,
+        gridHeight: 5,
+        teleportSnake: false,
+        initialCherryCount: 1,
+    });
 
     return (
         <div className="app">
