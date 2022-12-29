738126735d059bbe738221ae4fe05b699a1a13e2
ntorbinskiy <n.torbinskiy@gmail.com>
2022-12-29 13:55:11 +0200

    Remove useless imports at Snake.tsx

--- src/components/Snake/Snake.tsx
+++ src/components/Snake/Snake.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { state } from "../../state/state";
-import { SnakeHeadProps } from "../../types/state";
+
 import "./Snake.css";
 
 export const SnakeHead = () => {
@@ -8,20 +7,19 @@ export const SnakeHead = () => {
         <div
             className="snake-head"
             style={{
-                height: `${state.gridHeight * 10}px`,
-                width: `${state.gridWidth * 10}px`,
+                height: `100%`,
+                width: `100%`,
             }}
         ></div>
     );
 };
 export const SnakeBody = () => {
-    const cell = document.querySelector("div.cell");
     return (
         <div
             className="snake-body"
             style={{
-                height: `${cell?.clientHeight}px`,
-                width: `${cell?.clientWidth}px`,
+                height: `100%`,
+                width: `100%`,
             }}
         ></div>
     );
